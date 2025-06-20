5cc4f4c5f06cfe37a76e1bab37403232f2ffaa20
Robin Füllgraf <robin@fuellgraf.dev>
2025-06-21 01:38:15 +0200

    add compiler driver

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,84 @@
-use std::{cell::RefCell, error::Error, rc::Rc};
+use std::{error::Error, ffi::OsString, io::Write, process::Command};
 
 use chumsky::Parser;
 use parse::lexer::lexer;
+use tempfile::Builder;
 
-use crate::parse::{
-    source_file_parser::source_file_parser,
-    value_parser::{EmitEnvironment, GoMethodDef, GoTypeDef, emit, value_expr_parser},
-};
+use crate::parse::{source_file_parser::source_file_parser, value_parser::EmitEnvironment};
 
 pub mod parse;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    if true {
-        // let src = "if (x(1,true, (1,2,3,\"ABC\", (50, 100)))) { (\"lol\", 1) } else { return; }";
-        // let src = "if ({@println(1); true}) {1} else {2}";
-        // let src = "{ let i: Int = 0; while(!(i == 5)) {i = i + 1;@println(i);} }";
-        let src = "{{ x: 1, y: \"lol\" };@println({ y: \"lol\", x: \"fisch\" });}";
-        let src = "fun abc() {}fun x(){}use x;";
-        // let src = "{ let x: Int = 0; while (x ) }";
-        // let src = "{i = 10;}";
+    let mut keep_go = false;
+    let mut custom_out_name = None::<String>;
+    let mut file_name = None::<String>;
 
-        // let mut i = 0;
-        // while {println!("{i}"); i+= 1; if i == 500 {return Ok(());} i < 1000} {
-        //     dbg!(2);
-        // }
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
 
-        let src = std::fs::read_to_string(&std::env::args().nth(1).unwrap()).unwrap();
-        let lex = lexer().parse(&src).unwrap();
-        {
-            let parsed = dbg!(source_file_parser().parse(&lex).unwrap());
-
-            let emit_env = EmitEnvironment::default();
-            let functions = parsed.function_definitions.iter().map(|x| x.emit(emit_env.clone()))
-                .collect::<Vec<_>>().join("\n");
-
-            std::fs::write("generated.go", &format!("package main\n{}\n{}\n", emit_env.emit_imports_and_types(), functions)).unwrap();
-
-            return Ok(());
+    let mut i = 0;
+    while i < args.len() {
+        let arg = &args[i];
+        if arg == "--keep-go" {
+            keep_go = true;
+        } else if arg == "-o" {
+            custom_out_name = Some(
+                args.get(i + 1)
+                    .expect("Did provide argument for -o")
+                    .clone(),
+            );
+            i += 1;
+        } else {
+            if file_name.is_some() {
+                panic!("Error: reprovided file name");
+            }
+            file_name = Some(arg.to_owned());
         }
-        let parse = value_expr_parser().parse(&lex).unwrap();
-        let emit_env = EmitEnvironment {
-            imports: Rc::new(RefCell::new(Vec::new())),
-            types: Rc::new(RefCell::new(Vec::new())),
-            var_counter: Rc::new(RefCell::new(0)),
-        };
-        let emitted = emit(parse, emit_env.clone());
-
-        let types = [
-            GoTypeDef::Struct {
-                name: "HasX".into(),
-                fields: vec![("x".into(), "interface{}".into())],
-                methods: vec![GoMethodDef {
-                    name: "GetX".into(),
-                    body: vec!["return self.x".into()],
-                    params: vec![],
-                    return_type: Some("interface{}".into()),
-                }],
-            },
-            GoTypeDef::Interface {
-                name: "HasX_Any".into(),
-                methods: vec![GoMethodDef {
-                    name: "GetX".into(),
-                    body: vec!["return self.x".into()],
-                    params: vec![],
-                    return_type: Some("interface{}".into()),
-                }],
-            },
-        ];
-        std::fs::write(
-            "outgen.go",
-            types[0]
-                .emit()
-                .into_iter()
-                .reduce(|acc, x| format!("{acc}{x}"))
-                .unwrap(),
-        )
-        .unwrap();
-        std::fs::write(
-            "outgen_2.go",
-            types[1]
-                .emit()
-                .into_iter()
-                .reduce(|acc, x| format!("{acc}{x}"))
-                .unwrap(),
-        )
-        .unwrap();
-
-        emit_env.imports.borrow_mut().push("fmt".to_string());
-        emit_env.imports.borrow_mut().push("io".to_string());
-        let t = emit_env.emit_imports_and_types();
-
-        std::fs::write("outgen.go", format!("{}{}", t, emitted.0.join(""))).unwrap();
-        return Ok(());
+        i += 1;
     }
 
-    let source = Box::leak(Box::new("duck { x: String };"));
-    let lexer_parse_result = lexer().parse(source);
-    if !lexer_parse_result.has_output() {
-        panic!("Error while lexing");
+    let target_path = file_name.expect("No file name provided");
+    let src = std::fs::read_to_string(&target_path).expect("Could not read file");
+    let lex = lexer().parse(&src).into_result().expect("Lex error");
+    let parse = source_file_parser()
+        .parse(&lex)
+        .into_result()
+        .expect("Parse error");
+    let env = EmitEnvironment::new();
+    let functions = parse
+        .function_definitions
+        .iter()
+        .map(|x| x.emit(env.clone()))
+        .collect::<Vec<_>>()
+        .join("\n");
+    let out_text = format!(
+        "package main\n{}\n{}\n",
+        env.emit_imports_and_types(),
+        functions
+    );
+
+    let mut tmp_file = Builder::new()
+        .prefix("duck_gen")
+        .suffix(".go")
+        .tempfile_in(".")
+        .expect("Could not create tempfile");
+
+    if keep_go {
+        tmp_file.disable_cleanup(true);
     }
 
-    let tokens = lexer_parse_result.output().unwrap().as_slice();
-    dbg!(tokens);
+    write!(tmp_file, "{out_text}").expect("Could not write tmp file");
+    tmp_file.flush().expect("Could not flush tmp file");
 
-    // let type_parser_result = type_parser().parse(tokens.iter().map(|(token, span)| token).collect());
-    // if type_parser_result.has_errors() {
-    //  type_parser_result.errors().for_each(|err| println!("Error => {}", err));
-    // }
-
-    // if !type_parser_result.has_output() {
-    //panic!("Error while type parsing");
-    //}
-
-    // let type_expressions = type_parser_result.output();
-
+    Command::new("go")
+        .args([
+            OsString::from("build"),
+            OsString::from("-o"),
+            custom_out_name
+                .map(OsString::from)
+                .unwrap_or(OsString::from("duck_out")),
+            tmp_file.path().as_os_str().to_owned(),
+        ])
+        .spawn()?
+        .wait()?;
     Ok(())
 }
