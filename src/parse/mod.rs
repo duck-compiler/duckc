6848655cccab1963296b1efdbb84c3c5c316d507
Mvmo <mvmogo@gmail.com>
2025-06-21 05:22:25 +0200

    chore: fmt

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,8 +1,8 @@
-pub mod type_parser;
 pub mod assignment_and_declaration_parser;
-pub mod lexer;
 pub mod function_parser;
-pub mod value_parser;
-pub mod statement_parser;
-pub mod use_statement_parser;
+pub mod lexer;
 pub mod source_file_parser;
+pub mod statement_parser;
+pub mod type_parser;
+pub mod use_statement_parser;
+pub mod value_parser;
