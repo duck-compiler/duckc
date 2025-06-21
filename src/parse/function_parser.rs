ca2923832d5b98857373f8f6f2ac1d4c145f575f
Mvmo <mvmogo@gmail.com>
2025-06-21 05:21:57 +0200

    chore: go fmt

--- src/parse/function_parser.rs
+++ src/parse/function_parser.rs
@@ -2,7 +2,11 @@ use chumsky::prelude::*;
 
 use crate::parse::value_parser::{EmitEnvironment, emit};
 
-use super::{lexer::Token, type_parser::{type_expression_parser, TypeExpression}, value_parser::{value_expr_parser, ValueExpr}};
+use super::{
+    lexer::Token,
+    type_parser::{TypeExpression, type_expression_parser},
+    value_parser::{ValueExpr, value_expr_parser},
+};
 
 pub type Param = (String, TypeExpression);
 
@@ -21,7 +25,7 @@ impl Default for FunctionDefintion {
             name: Default::default(),
             return_type: Default::default(),
             params: Some(Default::default()),
-            value_expr: ValueExpr::Block(vec![ValueExpr::Tuple(vec![])])
+            value_expr: ValueExpr::Block(vec![ValueExpr::Tuple(vec![])]),
         }
     }
 }
@@ -34,16 +38,27 @@ impl FunctionDefintion {
         }
         let body = emitted_body.join("");
         [
-            format!("func {}({}) {} {}\n",
+            format!(
+                "func {}({}) {} {}\n",
                 self.name,
-                self.params.as_ref()
-                    .map(|x| x.iter().map(|x| format!("{} {}", x.0, x.1.emit().0)).collect::<Vec<_>>().join(", "))
+                self.params
+                    .as_ref()
+                    .map(|x| x
+                        .iter()
+                        .map(|x| format!("{} {}", x.0, x.1.emit().0))
+                        .collect::<Vec<_>>()
+                        .join(", "))
                     .unwrap_or_default(),
-                self.return_type.as_ref().map(|x| x.emit().0).unwrap_or_default(),
-            "{"),
+                self.return_type
+                    .as_ref()
+                    .map(|x| x.emit().0)
+                    .unwrap_or_default(),
+                "{"
+            ),
             body,
             "}\n".to_string(),
-        ].join("")
+        ]
+        .join("")
     }
 }
 
@@ -62,12 +77,14 @@ pub fn lambda_function_expr_parser<'src>() -> impl Parser<'src, &'src [Token], L
         .then(type_expression_parser())
         .map(|(identifier, type_expr)| (identifier, type_expr) as Param);
 
-    let params_parser = param_parser.separated_by(just(Token::ControlChar(',')))
+    let params_parser = param_parser
+        .separated_by(just(Token::ControlChar(',')))
         .allow_trailing()
         .collect::<Vec<Param>>()
         .or_not();
 
-    let return_type_parser = just(Token::ControlChar('-')).ignore_then(just(Token::ControlChar('>')))
+    let return_type_parser = just(Token::ControlChar('-'))
+        .ignore_then(just(Token::ControlChar('>')))
         .ignore_then(type_expression_parser());
 
     just(Token::ControlChar('('))
@@ -77,7 +94,11 @@ pub fn lambda_function_expr_parser<'src>() -> impl Parser<'src, &'src [Token], L
         .then_ignore(just(Token::ControlChar('=')))
         .then_ignore(just(Token::ControlChar('>')))
         .then(value_expr_parser())
-        .map(|((params, return_type), value_expr)| LambdaFunctionExpr { params: params.unwrap_or_default(), return_type, value_expr })
+        .map(|((params, return_type), value_expr)| LambdaFunctionExpr {
+            params: params.unwrap_or_default(),
+            return_type,
+            value_expr,
+        })
 }
 
 pub fn function_definition_parser<'src>() -> impl Parser<'src, &'src [Token], FunctionDefintion> {
@@ -86,12 +107,14 @@ pub fn function_definition_parser<'src>() -> impl Parser<'src, &'src [Token], Fu
         .then(type_expression_parser())
         .map(|(identifier, type_expr)| (identifier, type_expr) as Param);
 
-    let params_parser = param_parser.separated_by(just(Token::ControlChar(',')))
+    let params_parser = param_parser
+        .separated_by(just(Token::ControlChar(',')))
         .allow_trailing()
         .collect::<Vec<Param>>()
         .or_not();
 
-    let return_type_parser = just(Token::ControlChar('-')).ignore_then(just(Token::ControlChar('>')))
+    let return_type_parser = just(Token::ControlChar('-'))
+        .ignore_then(just(Token::ControlChar('>')))
         .ignore_then(type_expression_parser());
 
     just(Token::Function)
@@ -103,11 +126,18 @@ pub fn function_definition_parser<'src>() -> impl Parser<'src, &'src [Token], Fu
         .then(value_expr_parser())
         .map(|(((identifier, params), return_type), mut value_expr)| {
             value_expr = match value_expr {
-                ValueExpr::Duck(x) if x.is_empty() => ValueExpr::Block(vec![ValueExpr::Tuple(vec![])]),
+                ValueExpr::Duck(x) if x.is_empty() => {
+                    ValueExpr::Block(vec![ValueExpr::Tuple(vec![])])
+                }
                 x @ ValueExpr::Block(_) => x,
                 _ => panic!("Function must be block"),
             };
-            FunctionDefintion { name: identifier, return_type, params, value_expr }
+            FunctionDefintion {
+                name: identifier,
+                return_type,
+                params,
+                value_expr,
+            }
         })
 }
 
@@ -135,7 +165,9 @@ pub mod tests {
             assert_eq!(lexer_parse_result.has_errors(), false);
             assert_eq!(lexer_parse_result.has_output(), true);
 
-            let Some(tokens) = lexer_parse_result.into_output() else { unreachable!()};
+            let Some(tokens) = lexer_parse_result.into_output() else {
+                unreachable!()
+            };
 
             println!("typedef_parsing {valid_function_definition}");
             let typedef_parse_result = function_definition_parser().parse(tokens.as_slice());
@@ -143,8 +175,7 @@ pub mod tests {
             assert_eq!(typedef_parse_result.has_output(), true);
         }
 
-        let invalid_function_definitions = vec![
-        ];
+        let invalid_function_definitions = vec![];
 
         for invalid_function_definition in invalid_function_definitions {
             println!("lexing {invalid_function_definition}");
@@ -152,7 +183,9 @@ pub mod tests {
             assert_eq!(lexer_parse_result.has_errors(), false);
             assert_eq!(lexer_parse_result.has_output(), true);
 
-            let Some(tokens) = lexer_parse_result.into_output() else { unreachable!()};
+            let Some(tokens) = lexer_parse_result.into_output() else {
+                unreachable!()
+            };
 
             println!("typedef_parsing {invalid_function_definition}");
             let typedef_parse_result = function_definition_parser().parse(tokens.as_slice());
@@ -163,11 +196,8 @@ pub mod tests {
 
     #[test]
     fn test_lambda_function_defintion() {
-        let valid_lambda_function_definitions = vec![
-            "() => {}",
-            "() -> String => {}",
-            "() => { 5; }",
-        ];
+        let valid_lambda_function_definitions =
+            vec!["() => {}", "() -> String => {}", "() => { 5; }"];
 
         for valid_lambda_function_definition in valid_lambda_function_definitions {
             println!("lexing {valid_lambda_function_definition}");
@@ -175,7 +205,9 @@ pub mod tests {
             assert_eq!(lexer_parse_result.has_errors(), false);
             assert_eq!(lexer_parse_result.has_output(), true);
 
-            let Some(tokens) = lexer_parse_result.into_output() else { unreachable!()};
+            let Some(tokens) = lexer_parse_result.into_output() else {
+                unreachable!()
+            };
 
             println!("lambda parsing {valid_lambda_function_definition}");
             let lambda_parse_result = lambda_function_expr_parser().parse(tokens.as_slice());
@@ -183,8 +215,7 @@ pub mod tests {
             assert_eq!(lambda_parse_result.has_output(), true);
         }
 
-        let invalid_function_definitions = vec![
-        ];
+        let invalid_function_definitions = vec![];
 
         for invalid_function_definition in invalid_function_definitions {
             println!("lexing {invalid_function_definition}");
@@ -192,7 +223,9 @@ pub mod tests {
             assert_eq!(lexer_parse_result.has_errors(), false);
             assert_eq!(lexer_parse_result.has_output(), true);
 
-            let Some(tokens) = lexer_parse_result.into_output() else { unreachable!()};
+            let Some(tokens) = lexer_parse_result.into_output() else {
+                unreachable!()
+            };
 
             println!("typedef_parsing {invalid_function_definition}");
             let typedef_parse_result = function_definition_parser().parse(tokens.as_slice());
