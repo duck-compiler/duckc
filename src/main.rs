e1a13baf4a5c5d0d3d8b6cc13d7566622602ca52
Mvmo <mvmogo@gmail.com>
2025-06-26 23:48:56 +0200

    fix: correct test

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,12 @@
     clippy::only_used_in_recursion
 )]
 
-use std::{error::Error, ffi::OsString, io::Write, path::PathBuf, process::Command};
+use std::{error::Error, ffi::OsString, fs, io::{self, Write}, path::{Path, PathBuf}, process::Command};
 
 use chumsky::{Parser, error::Rich};
 use clap::{Parser as CliParser, arg, command};
-use parse::lexer::lexer;
+use parse::{lexer::{self, lexer, Token}, source_file_parser::SourceFile, Spanned};
 use semantics::typechecker::{self, TypeEnv};
-use tempfile::Builder;
 
 use crate::{
     emit::ir::join_ir,
@@ -22,6 +21,7 @@ use crate::{
         value_parser::{empty_range, value_expr_parser},
     },
 };
+use lazy_static::lazy_static;
 
 pub mod emit;
 pub mod fixup;
@@ -44,12 +44,12 @@ struct CompilerArgs {
     emit_go: bool,
 }
 
-fn test_error_messages() {
-    let src = "if (1";
-
-    let out = lexer("test", src).parse(src).into_result().unwrap();
-    let (_out, errors) = value_expr_parser(make_input)
-        .parse(make_input(empty_range(), &out))
+fn lex(
+    file_name: &'static str,
+    file_contents: &'static str,
+) -> Vec<Spanned<Token>> {
+    let (lex, lex_errors) = lexer(file_name, file_contents)
+        .parse(file_contents)
         .into_output_errors();
 
     errors.into_iter().for_each(|e| {
@@ -82,91 +82,145 @@ fn main() -> Result<(), Box<dyn Error>> {
         .into_output_errors();
     lex_errors.into_iter().for_each(|e| {
         parse_failure(
-            target_path_name,
+            file_name,
             &Rich::<&str, SS>::custom(
                 SS {
                     start: e.span().start,
                     end: e.span().end,
                     context: crate::parse::Context {
-                        file_name: target_path_name,
-                        file_contents: src,
+                        file_name,
+                        file_contents,
                     },
                 },
                 "Lex Error",
             ),
-            &src,
+            file_contents,
         );
     });
 
-    let lex = lex.unwrap();
+    lex.unwrap()
+}
+
+fn parse_src_file(
+    src_file: &PathBuf,
+    src_file_name: &'static str,
+    src_file_file_contents: &'static str,
+    tokens: Vec<Spanned<Token>>
+) -> SourceFile {
     let (src_file, parse_errors) = source_file_parser(
-        {
-            let mut p = target_path.clone();
-            p.pop();
-            p
-        },
-        make_input,
-    )
-    .parse(make_input(
-        SS {
-            start: 0,
-            end: src.len(),
-            context: Context {
-                file_name: target_path_name,
-                file_contents: src,
-            },
-        },
-        &lex,
-    ))
-    .into_output_errors();
+           {
+               let mut src_file_clone = src_file.clone();
+               src_file_clone.pop();
+               src_file_clone
+           },
+           make_input,
+       )
+       .parse(make_input(
+           SS {
+               start: 0,
+               end: src_file_file_contents.len(),
+               context: Context {
+                   file_name: src_file_name,
+                   file_contents: src_file_file_contents,
+               },
+           },
+           &tokens,
+       ))
+       .into_output_errors();
 
     parse_errors.into_iter().for_each(|e| {
-        parse_failure(target_path_name, &e, &src);
-    });
+            parse_failure(src_file_name, &e, &src_file_file_contents);
+        });
 
-    let mut source_file = dbg!(src_file.unwrap());
-    source_file = source_file.flatten();
-    for f in &source_file.function_definitions {
-        dbg!(f.value_expr.1.context);
-    }
+    src_file.unwrap().flatten()
+}
 
+fn typecheck(src_file_ast: &mut SourceFile) -> TypeEnv {
     let mut type_env = TypeEnv::default();
-    typechecker::typeresolve_source_file(&mut source_file, &mut type_env);
+    typechecker::typeresolve_source_file(src_file_ast, &mut type_env);
 
+    type_env
+}
+
+fn emit_go_code(src_file_ast: &mut SourceFile, type_env: &mut TypeEnv) -> String {
     let out_text = join_ir(&source_file.emit("main".into(), &mut type_env));
+    let env = EmitEnvironment::new();
+    src_file_ast
+        .use_statements
+        .iter()
+        .filter_map(|x| match x.to_owned() {
+            UseStatement::Go(path, alias) => Some(GoImport { path, alias }),
+            _ => None,
+        })
+        .for_each(|x| {
+            env.push_import(x);
+        });
 
-    let mut tmp_file = Builder::new()
-        .rand_bytes(3)
-        .prefix(
-            target_path
-                .file_name()
-                .expect("didnt provide file name")
-                .to_str()
-                .expect("not valid utf 8"),
-        )
-        .suffix(".go")
-        .tempfile_in(".")
-        .expect("Could not create tempfile");
+    let _functions = src_file_ast
+        .function_definitions
+        .iter()
+        .map(|x| x.emit(env.clone(), type_env))
+        .collect::<Vec<_>>()
+        .join("\n");
 
-    tmp_file.disable_cleanup(!compiler_args.emit_go);
+    let generated_go_src_str = src_file_ast.clone().emit("main".into(), type_env);
 
-    write!(tmp_file, "{out_text}").expect("Could not write tmp file");
-    tmp_file.flush().expect("Could not flush tmp file");
+    generated_go_src_str
+}
+
+lazy_static! {
+    static ref DOT_DUCK_DIR: PathBuf = {
+        let duck_dir = Path::new("./.duck");
+        if duck_dir.exists() {
+            return duck_dir.to_path_buf();
+        }
+
+        if let Err(err) = fs::create_dir(duck_dir) {
+            dbg!(err);
+            println!("Couldn't create .duck dir");
+        }
+
+        return duck_dir.to_path_buf();
+    };
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    ensure_duck_dir_exists()?;
+
+    let compiler_args = CompilerArgs::parse();
+
+    let src_file: PathBuf = compiler_args.input_file;
+    let src_file_name: &'static str = src_file
+        .file_name()
+        .expect("couldn't read file name")
+        .to_str()
+        .expect("invalid utf-8 string")
+        .to_string()
+        .leak();
+    let src_file_file_contents: &'static str = fs::read_to_string(&src_file)
+        .expect("couldn't read file")
+        .to_string()
+        .leak();
+
+    let tokens = lex(src_file_name, src_file_file_contents);
+    let mut src_file_ast = parse_src_file(&src_file, src_file_name, src_file_file_contents, tokens);
+    let mut type_env = typecheck(&mut src_file_ast);
+    let go_code = emit_go_code(&mut src_file_ast, &mut type_env);
 
     let out_file = compiler_args
         .o
         .map(OsString::from)
         .unwrap_or(OsString::from("duck_out"));
 
-    Command::new("go")
-        .args([
-            OsString::from("build"),
-            OsString::from("-o"),
-            out_file.clone(),
-            tmp_file.path().as_os_str().to_owned(),
-        ])
-        .spawn()?
-        .wait()?;
+    // Command::new("go")
+    //     .args([
+    //         OsString::from("build"),
+    //         OsString::from("-o"),
+    //         out_file.clone(),
+    //         tmp_file.path().as_os_str().to_owned(),
+    //     ])
+    //     .spawn()?
+    //     .wait()?;
 
     println!(
         "Successfully compiled binary {}",
