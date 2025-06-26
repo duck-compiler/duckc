9540733633b541acdd5010085197ed42e4005e36
Mvmo <mvmogo@gmail.com>
2025-06-26 16:54:24 +0200

    fix: allow dead code

--- src/fixup/main_fun_fixup.rs
+++ src/fixup/main_fun_fixup.rs
@@ -1,5 +1,6 @@
 use crate::parse::function_parser::FunctionDefintion;
 
+#[allow(dead_code)]
 fn fixup_main_fun(fun_def: &mut FunctionDefintion) {
     if fun_def.name != "main" { return }
     // TODO: resolve returns in main function to use the exit() fn of go, or wrap the code of the main function into a second function which then get's called inside a new main which just returns exit(other_main())
