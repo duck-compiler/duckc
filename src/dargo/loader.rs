9bbc8b946a80c52ac529398d60e77ad00c977385
Mvmo <mvmogo@gmail.com>
2025-06-29 20:05:02 +0200

    fix: correctly load dependencies

--- src/dargo/loader.rs
+++ src/dargo/loader.rs
@@ -1,5 +1,6 @@
 use colored::Colorize;
 use serde::Deserialize;
+use std::collections::HashMap;
 use std::fs;
 use std::path::{Path, PathBuf};
 use toml;
@@ -14,8 +15,10 @@ pub struct BinaryConfig {
 }
 
 #[derive(Debug, Clone, PartialEq, Deserialize)]
-pub struct DependencyConfig {
-    pub git_url: String,
+#[serde(untagged)]
+pub enum Dependency {
+    WithVersion(String),
+    // WithConfig(DependencyConfig)
 }
 
 #[derive(Debug, Clone, PartialEq, Deserialize)]
@@ -23,7 +26,7 @@ pub struct ProjectConfig {
     pub name: String,
     #[serde(rename = "bin", default)]
     pub binaries: Vec<BinaryConfig>,
-    pub dependencies: Option<Vec<DependencyConfig>>,
+    pub dependencies: Option<HashMap<String, Dependency>>,
 }
 
 #[derive(Debug, Clone, Deserialize)]
@@ -51,19 +54,20 @@ pub fn load_dargo_config(
         return Err((message, ProjectLoadErrKind::MissingDuckToml));
     }
 
-    let file_content = fs::read_to_string(path).map_err(|read_error| {
-        let message = format!(
-            "{} Couldn't read dargo.toml.\n -> {read_error}",
-            " Error ".on_red().bright_white(),
-        );
+    let file_content = fs::read_to_string(path)
+        .map_err(|read_error| {
+            let message = format!(
+                "{} Couldn't read dargo.toml.\n -> {read_error}",
+                Tag::Err,
+            );
 
-        (message, ProjectLoadErrKind::FileRead)
-    })?;
+            (message, ProjectLoadErrKind::FileRead)
+        })?;
 
     let project_config = toml::from_str(&file_content).map_err(|parse_error| {
         let message = format!(
             "{} {} Couldn't parse dargo.toml file.\n -> {parse_error}",
-            " Error ".on_red().bright_white(),
+            Tag::Err,
             " TOML ".on_yellow().bright_white(),
         );
 
@@ -75,10 +79,16 @@ pub fn load_dargo_config(
 
 #[cfg(test)]
 mod tests {
-    use crate::dargo::init::init_project;
-
     use super::*;
     use std::io::Write;
+
+    fn assert_dependency_with_version(dependencies: &HashMap<String, Dependency>, name: &str, version: &str) {
+        assert!(dependencies.contains_key(name));
+        let dep = dependencies.get(name).unwrap();
+        assert!(matches!(dep, Dependency::WithVersion(..)));
+        let Dependency::WithVersion(actual_version) = dep;
+        assert_eq!(*actual_version, version);
+    }
 
     fn create_temp_file(file_name: &str, content: &str) -> PathBuf {
         let mut path = std::env::temp_dir();
@@ -103,12 +113,9 @@ mod tests {
             version = "0.9.0"
             file = "./src/another.duck"
 
-            [[dependencies]]
-            git_url = "https://github.com/some/fetch.git"
-
-            [[dependencies]]
-            git_url = "https://github.com/another/dep.git"
-        "#;
+            [dependencies]
+            "some/fetch" = "1.0.0"
+            "#;
         let file_path = create_temp_file("valid_dargo.toml", toml_content);
 
         let result = load_dargo_config(Some(file_path.clone()));
@@ -125,16 +132,12 @@ mod tests {
         assert_eq!(config.binaries[1].version, "0.9.0");
         assert_eq!(config.binaries[1].file, PathBuf::from("./src/another.duck"));
 
-        if let Some(dependencies) = config.dependencies {
-            assert_eq!(dependencies.len(), 2);
-            assert_eq!(dependencies[0].git_url, "https://github.com/some/fetch.git");
-            assert_eq!(
-                dependencies[1].git_url,
-                "https://github.com/another/dep.git"
-            );
-        } else {
-            panic!("didn't find any dependencies");
-        }
+        assert!(matches!(config.dependencies, Some(..)));
+        let Some(dependencies) = config.dependencies else { unreachable!() };
+
+        assert_eq!(dependencies.len(), 1);
+
+        assert_dependency_with_version(&dependencies, "some/fetch", "1.0.0");
 
         fs::remove_file(file_path).unwrap();
     }
@@ -157,9 +160,9 @@ mod tests {
             version = "1.0.0"
             file = "./src/main.duck"
             [dependencies]
-            git_url = "x"
-        "#
-        .trim();
+            test
+            "#
+            .trim();
         let file_path = create_temp_file("malformed_dargo.toml", malformed_content);
 
         let result = load_dargo_config(Some(file_path.clone()));
@@ -205,14 +208,11 @@ mod tests {
         let toml_content = r#"
             name="Project With Complex Dependencies"
 
-            [[dependencies]]
-            git_url = "ssh://git@github.com/org/repo.git"
+            [dependencies]
+            "first/fetch" = "3.0.0"
+            "second/fetch" = "2.0.0"
+            "third/fetch" = "1.0.0"
 
-            [[dependencies]]
-            git_url = "file:///local/path/to/repo"
-
-            [[dependencies]]
-            git_url = "git@bitbucket.org:user/project.git"
         "#;
         let file_path = create_temp_file("complex_deps_dargo.toml", toml_content);
 
@@ -222,47 +222,13 @@ mod tests {
 
         assert_eq!(config.name, "Project With Complex Dependencies");
 
-        if let Some(dependencies) = config.dependencies {
-            assert_eq!(dependencies.len(), 3);
-            assert_eq!(dependencies[0].git_url, "ssh://git@github.com/org/repo.git");
-            assert_eq!(dependencies[1].git_url, "file:///local/path/to/repo");
-            assert_eq!(
-                dependencies[2].git_url,
-                "git@bitbucket.org:user/project.git"
-            );
-        } else {
-            panic!("didn't have any dependencies")
-        }
 
-        fs::remove_file(file_path).unwrap();
-    }
+        let Some(dependencies) = config.dependencies else {unreachable!()};
+        assert_eq!(dependencies.len(), 3);
 
-    #[test]
-    fn test_bootstrap_default_project_env() {
-        let file_path = PathBuf::from("default_dargo.toml");
-
-        let bootstrap_result = init_project(Some(file_path.clone()));
-        assert!(bootstrap_result.is_ok());
-
-        let load_result = load_dargo_config(Some(file_path.clone()));
-        assert!(load_result.is_ok());
-        let config = load_result.unwrap();
-
-        assert_eq!(config.name, "My New Project");
-        assert_eq!(config.binaries.len(), 1);
-        assert_eq!(config.binaries[0].name, "my_app");
-        assert_eq!(config.binaries[0].version, "0.1.0");
-        assert_eq!(config.binaries[0].file, PathBuf::from("./src/main.duck"));
-
-        if let Some(dependencies) = config.dependencies {
-            assert_eq!(dependencies.len(), 1);
-            assert_eq!(
-                dependencies[0].git_url,
-                "https://github.com/example/default_dep.git"
-            );
-        } else {
-            panic!("didn't have any deps");
-        }
+        assert_dependency_with_version(&dependencies, "first/fetch", "3.0.0");
+        assert_dependency_with_version(&dependencies, "second/fetch", "2.0.0");
+        assert_dependency_with_version(&dependencies, "third/fetch", "1.0.0");
 
         fs::remove_file(file_path).unwrap();
     }
