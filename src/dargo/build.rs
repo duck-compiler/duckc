8ef2e91e72f68620614d4c292c43d56487185d7e
Mvmo <mvmogo@gmail.com>
2025-06-29 20:06:32 +0200

    chore: start of dargo build

---
+++ src/dargo/build.rs
@@ -0,0 +1,20 @@
+use crate::tags::Tag;
+use super::cli::BuildArgs;
+use super::loader::{load_dargo_config, ProjectLoadErrKind};
+
+#[derive(Debug)]
+pub enum BuildErrKind {
+    CargoConfigLoad(ProjectLoadErrKind)
+}
+
+pub fn build(build_args: &BuildArgs) -> Result<(), (String, BuildErrKind)> {
+    let dargo_config = load_dargo_config(None)
+        .map_err(|err| (
+            err.0,
+            BuildErrKind::CargoConfigLoad(err.1),
+        ))?;
+
+    dbg!(dargo_config);
+
+    Ok(())
+}
