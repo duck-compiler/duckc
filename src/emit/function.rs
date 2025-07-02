d996409261a9766af845883b1ea8a5ba4064ab86
Mvmo <mvmogo@gmail.com>
2025-07-02 16:55:03 +0200

    fix: set return type of function to none for emit

--- src/emit/function.rs
+++ src/emit/function.rs
@@ -23,9 +23,11 @@ impl FunctionDefintion {
                 .iter()
                 .map(|(name, (ty, _))| (name.clone(), ty.as_go_type_annotation(type_env)))
                 .collect::<Vec<_>>(),
-            self.return_type
-                .as_ref()
-                .map(|x| x.0.as_go_type_annotation(type_env)),
+            if self.name == "main" { None } else {
+                self.return_type
+                    .as_ref()
+                    .map(|x| x.0.as_go_type_annotation(type_env))
+            },
             emitted_body,
         )
     }
