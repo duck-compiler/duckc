3ba61bd2cea610b4752a9d91301ee16df279bc87
Mvmo <mvmogo@gmail.com>
2025-09-19 01:17:50 +0200

    fix: windows :)

--- src/dargo/build.rs
+++ src/dargo/build.rs
@@ -1,11 +1,17 @@
 use std::io::ErrorKind as IOErrKind;
 use std::path::{Path, PathBuf};
-use std::{env, fs, os};
+use std::{env, fs};
 
 use crate::DARGO_DOT_DIR;
 use crate::cli::git_cli::{self, GitCliErrKind};
 use crate::dargo::cli::CompileArgs;
 use crate::tags::Tag;
+
+#[cfg(target_os = "windows")]
+use std::os::windows::fs::symlink_dir;
+
+#[cfg(not(target_os = "windows"))]
+use std::os::unix::fs::symlink;
 
 use super::cli::BuildArgs;
 use super::compile::{self, CompileErrKind};
@@ -109,8 +115,15 @@ pub fn build(build_args: &BuildArgs) -> Result<BuildOutput, (String, BuildErrKin
             current_dir.push(format!(".dargo/git/{module_name}/src"));
             let absolute_src_dir = current_dir;
 
-            // TODO: windows :(
-            os::unix::fs::symlink(absolute_src_dir, target_dir).map_err(|err| {
+
+            #[cfg(target_os = "windows")]
+            let result = symlink_dir(absolute_src_dir, target_dir);
+
+            #[cfg(not(target_os = "windows"))]
+            let result = symlink(absolute_src_dir, target_dir);
+
+
+            result.map_err(|err| {
                 (
                     format!("{}{} error creating symlink - {err}", Tag::IO, Tag::Err,),
                     BuildErrKind::IOErr(err.kind()),
