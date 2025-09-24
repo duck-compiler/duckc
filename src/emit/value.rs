fae4ae81e6bf72e90399635bfa90ca4ecc51929b
Robin Füllgraf <robin@fuellgraf.dev>
2025-09-24 11:40:33 +0200

    fix: don't tread struct differently when taking ref

--- src/emit/value.rs
+++ src/emit/value.rs
@@ -378,25 +378,21 @@ impl ValueExpr {
             }
             ValueExpr::Ref(v) | ValueExpr::RefMut(v) => {
                 let t = TypeExpr::from_value_expr_resolved_type_name(&*v, type_env);
-                if false && matches!(t, TypeExpr::Struct(..)) {
-                    v.0.emit(type_env, env, span)
+                let ptr_type = format!("*{}", t.as_go_type_annotation(type_env));
+                let (mut emit_instr, emit_res) = v.0.emit(type_env, env, span);
+                if let Some(emit_res) = emit_res {
+                    let var_name = env.new_var();
+                    let ptr_var_decl = [
+                        IrInstruction::VarDecl(var_name.clone(), ptr_type),
+                        IrInstruction::VarAssignment(
+                            var_name.clone(),
+                            IrValue::Pointer(Box::new(emit_res)),
+                        ),
+                    ];
+                    emit_instr.extend(ptr_var_decl);
+                    (emit_instr, Some(IrValue::Var(var_name)))
                 } else {
-                    let ptr_type = format!("*{}", t.as_go_type_annotation(type_env));
-                    let (mut emit_instr, emit_res) = v.0.emit(type_env, env, span);
-                    if let Some(emit_res) = emit_res {
-                        let var_name = env.new_var();
-                        let ptr_var_decl = [
-                            IrInstruction::VarDecl(var_name.clone(), ptr_type),
-                            IrInstruction::VarAssignment(
-                                var_name.clone(),
-                                IrValue::Pointer(Box::new(emit_res)),
-                            ),
-                        ];
-                        emit_instr.extend(ptr_var_decl);
-                        (emit_instr, Some(IrValue::Var(var_name)))
-                    } else {
-                        (emit_instr, None)
-                    }
+                    (emit_instr, None)
                 }
             }
             ValueExpr::Sub(lhs, rhs) => {
